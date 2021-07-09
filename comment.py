6453c6391e09a04296505607fd9bcaba1f0974fd
Derek <derekforgione@gmail.com>
2021-07-09 16:32:46 -0700

    Created comment.py

---
+++ comment.py
@@ -0,0 +1,13 @@
+"""
+This is a command line program to generate comments for
+various programming languages.
+
+Currently Supported Languages:
+	None
+"""
+
+import sys
+
+if __name__ == '__main__':
+	for arg in sys.argv:
+		print(arg)
