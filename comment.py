ecf143afbc0d009e5b3861f2a0bbbb30d8f6ec23
Derek <derekforgione@gmail.com>
2021-07-10 19:17:27 -0700

    Added comment detailing the workings of the script

--- comment.py
+++ comment.py
@@ -8,6 +8,30 @@ Currently Supported Languages:
 
 import sys
 
+# Because this program will use arguments like a command
+# stack, it will have to parse the arguments without 
+# external libraries.
+
+# Usage:
+# 	Forms of input:
+#	[Indented]
+# 		0:"No Indentation"
+#		"No Indentation"
+# 		1:"One indentation unit."
+# 		2:"Two indentation units."
+#		3:"Three indentation units."
+#	[io]
+#		~"file.txt"
+#		?"Prompt>"
+#		@"Prompt"
+#	'~' represents reading from a file.
+#	'?' represents reading from the input stream.
+#	'@' represents reading from an external editor, such as VS Code.
+# comment.py [ "Header" 1:"Description" table(width=80)[ row[ left:"Left Justified" center:"Center Justified" right:"Right Justified" ] ] ]
+
+##############################################################
+#                        #                                   #
+
 if __name__ == '__main__':
 	for arg in sys.argv:
 		print(arg)
