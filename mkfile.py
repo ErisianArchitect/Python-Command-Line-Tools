ac954b476bcb0a0e9128cdc2a788832bef5aff07
Derek <derekforgione@gmail.com>
2021-07-15 21:56:40 -0700

    Updated to add editor option.

--- mkfile.py
+++ mkfile.py
@@ -1,5 +1,5 @@
 """
-This script attempts to mimic the touch command in Linux.
+This script creates one or more files at the specified locations.
 I wrote this script because I use Windows 10, and it's
 nice to have a way to create empty files from the command line.
 """
@@ -10,10 +10,19 @@ import pathlib
 import shutil
 import subprocess
 
+# First check for the DEFAULTEDITOR environtment variable.
+# If that is not found, we check if VS Code is installed
+# by looking for the 'code' command.
+# If VS Code is not found, we find notepad, which should
+# be on all Windows machines.
+_default_editor = os.environ.get('DEFAULTEDITOR', None) or shutil.which('code') or shutil.which('notepad')
+
 @click.command()
+@click.option('--editor', 'editor', required=False, default=_default_editor, help="Default editor to open if [-o|--open] flag is set.")
 @click.option('-o', '--open', 'open_in_editor', required = False, is_flag = True, default = False, help="Open in external editor. (VS Code)")
 @click.argument('paths', required=True, nargs=-1)
 def main(
+    editor = _default_editor,
     open_in_editor = False,
     paths = ()
     ):
@@ -37,7 +46,7 @@ def main(
         # In the future, we can add another option for what command to run
         # to open the files.
         if open_in_editor:
-            subprocess.call([shutil.which('code'), abs_path])
+            subprocess.call([editor, abs_path])
     # We want to tell the user that their file(s) have been created.
     # We check if there is only one file or multiple files that have been
     # created in order determine whether or not 'File' needs to be plural
